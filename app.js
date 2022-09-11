d0817d76b21bafd81a66ba971413f0f99e8bdb35
Selami Demir <selamidemir@gmail.com>
2022-09-11 14:25:12 +0300

    Ejs modülü eklendi. Arayüz modüle göre düzenlendi. Sayfa routları oluşturuldu.

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+app.get('/photos', (req, res) => {
+    res.render('photos');
+});
+
+
 app.listen(port, () => console.log(`Sunucu ${port} nolu porttan başlatıldı.`));
